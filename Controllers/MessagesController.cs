f7a68123be7b1e65fd2b0c1980c8e2e647ba374b
Alexandr Köln <koelnfrei@gmail.com>
2017-07-18 09:50:11 +0300

    co

--- Controllers/MessagesController.cs
+++ Controllers/MessagesController.cs
@@ -2,37 +2,25 @@
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
+using CrownberryBot.Dialogs;
 using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Connector;
-using System;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Net.Http.Headers;
-using System.Text.RegularExpressions;
-using ImageCaption.Services;
-using Microsoft.ProjectOxford.Vision;
 
 namespace CrownberryBot
 {
     [BotAuthentication]
     public class MessagesController : ApiController
     {
-
         /// <summary>
-        /// POST: api/Messages
-        /// Receive a message from a user and reply to it
+        ///     POST: api/Messages
+        ///     Receive a message from a user and reply to it
         /// </summary>
-        public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
+        public async Task<HttpResponseMessage> Post([FromBody] Activity activity)
         {
             if (activity.Type == ActivityTypes.Message)
-            {
-                await Conversation.SendAsync(activity, () => new Dialogs.RootDialog());
-            }
+                await Conversation.SendAsync(activity, () => new RootDialog());
             else
-            {
                 HandleSystemMessage(activity);
-            }
             var response = Request.CreateResponse(HttpStatusCode.OK);
             return response;
         }
@@ -47,10 +35,6 @@ namespace CrownberryBot
                     break;
                 case ActivityTypes.ConversationUpdate:
                     // Greet the user the first time the bot is added to a conversation.
-                    if (activity.MembersAdded.Any(m => m.Id == activity.Recipient.Id))
-                    {
-                        break;
-                    }
 
                     break;
                 case ActivityTypes.ContactRelationUpdate:
