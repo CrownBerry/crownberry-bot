08b80f09b20fe4b276a353b299f9b422a2e84833
Alexandr Köln <koelnfrei@gmail.com>
2017-07-18 09:51:30 +0300

    co

--- Controllers/MessagesController.cs
+++ Controllers/MessagesController.cs
@@ -2,9 +2,9 @@
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
-using CrownberryBot.Dialogs;
 using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Connector;
+using System;
 
 namespace CrownberryBot
 {
@@ -12,39 +12,47 @@ namespace CrownberryBot
     public class MessagesController : ApiController
     {
         /// <summary>
-        ///     POST: api/Messages
-        ///     Receive a message from a user and reply to it
+        /// POST: api/Messages
+        /// Receive a message from a user and reply to it
         /// </summary>
-        public async Task<HttpResponseMessage> Post([FromBody] Activity activity)
+        public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
         {
             if (activity.Type == ActivityTypes.Message)
-                await Conversation.SendAsync(activity, () => new RootDialog());
+            {
+                await Conversation.SendAsync(activity, () => new Dialogs.RootDialog());
+            }
             else
+            {
                 HandleSystemMessage(activity);
+            }
             var response = Request.CreateResponse(HttpStatusCode.OK);
             return response;
         }
 
-        private async Task<Activity> HandleSystemMessage(Activity activity)
+        private Activity HandleSystemMessage(Activity message)
         {
-            switch (activity.Type)
+            if (message.Type == ActivityTypes.DeleteUserData)
             {
-                case ActivityTypes.DeleteUserData:
-                    // Implement user deletion here
-                    // If we handle user deletion, return a real message
-                    break;
-                case ActivityTypes.ConversationUpdate:
-                    // Greet the user the first time the bot is added to a conversation.
-
-                    break;
-                case ActivityTypes.ContactRelationUpdate:
-                    // Handle add/remove from contact lists
-                    break;
-                case ActivityTypes.Typing:
-                    // Handle knowing that the user is typing
-                    break;
-                case ActivityTypes.Ping:
-                    break;
+                // Implement user deletion here
+                // If we handle user deletion, return a real message
+            }
+            else if (message.Type == ActivityTypes.ConversationUpdate)
+            {
+                // Handle conversation state changes, like members being added and removed
+                // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
+                // Not available in all channels
+            }
+            else if (message.Type == ActivityTypes.ContactRelationUpdate)
+            {
+                // Handle add/remove from contact lists
+                // Activity.From + Activity.Action represent what happened
+            }
+            else if (message.Type == ActivityTypes.Typing)
+            {
+                // Handle knowing tha the user is typing
+            }
+            else if (message.Type == ActivityTypes.Ping)
+            {
             }
 
             return null;
